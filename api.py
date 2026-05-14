21e5de3601aa4b62f5cebd5629596498230d592a
A27711 <muhammadbaltabayev@gmail.com>
2026-05-14 14:15:05 +0500

    CORS butunlay ochiq holatga keltirildi

--- api.py
+++ api.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://crm-rysx.vercel.app"], # DING! Aynan shu manzil
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
